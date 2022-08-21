7c7111781c6c9f7eb7a95f8d3bfcf43dc4c7de4e
Eduardo Flores <efloresm@minsait.com>
2022-08-21 20:57:26 +0200

    Created MoviesApiService, refactored MoviesController and removed EFCore packages and references about Startup class and all the project

---
+++ secure/aspnet-secure-microservices/Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MovieApiService(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
+            _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Comics",
+                    Title = "asd",
+                    Rating = "9.2",
+                    ImageUrl = "images/src",
+                    ReleaseDate = DateTime.Now,
+                    Owner = "edu"
+
+                });
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        
+
+        public Task<UserInfoViewModel> GetUserInfo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
